090805e994694d30fbe7542310702c38bf805e96
Austin Joseph <josephaustin7232003@gmail.com>
2025-12-01 17:05:13 -0500

    sudoku output grid

--- SudokuSolver.py
+++ SudokuSolver.py
@@ -7,8 +7,15 @@ for r in range(9):
     for c in range(9):
         sudoku_grid[r, c] = list(range(1, 10))  # Assign a new list to each cell
 
+
+sudoku_output = np.zeros((9, 9))
+
 print(sudoku_grid)
 
 # Access a specific cell and its list
 print(f"\nList in cell (0, 0): {sudoku_grid[0, 0]}")
 print(f"List in cell (4, 5): {sudoku_grid[4, 5]}")
+
+print(sudoku_output)
+
+
