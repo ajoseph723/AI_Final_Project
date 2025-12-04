1cdcf6c5ea9a7ccaf801bbfdbbb7bd4ed9d39d77
Austin Joseph <josephaustin7232003@gmail.com>
2025-12-04 12:30:08 -0500

    starting look ahead table setup

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 from SudokuSolver import generate_puzzle, board_to_numpy, calculate_dependent_cells, select_next, check_move, make_move
+import numpy as np
 
 app = Flask(__name__)
 
@@ -11,6 +12,9 @@ def index():
 def sudoku_data():
     puzzle = generate_puzzle()
     numpy_grid = board_to_numpy(puzzle.board)
+
+    
+    sudoku_look_ahead_table = np.empty((9, 9), dtype=object)
     return jsonify(numpy_grid.tolist())
 
 if __name__ == '__main__':
