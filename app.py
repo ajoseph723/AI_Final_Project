e44e3659efbae5d28450974452e8afe3157782f9
Austin Joseph <josephaustin7232003@gmail.com>
2025-12-04 12:43:59 -0500

    intial setup of look ahead table in app.py

--- app.py
+++ app.py
@@ -11,11 +11,42 @@ def index():
 @app.route('/sudoku_data')
 def sudoku_data():
     puzzle = generate_puzzle()
-    numpy_grid = board_to_numpy(puzzle.board)
+    sudoku_output = board_to_numpy(puzzle.board)
 
-    
+    #look ahead table orgiginal setup
     sudoku_look_ahead_table = np.empty((9, 9), dtype=object)
-    return jsonify(numpy_grid.tolist())
+
+    for r in range(9):
+        for c in range(9):
+
+            dependent_cells = calculate_dependent_cells(r, c, sudoku_output)
+
+            #create a dictionary for each cell
+            #constraints are possible values for the cell
+            #dependent_cells are cells that are affected by the value of cell (r, c)
+            cell_data = {
+                "constraints": [],
+                "dependent_cells": dependent_cells
+            }
+            if(sudoku_output[r, c] > 0):
+                set_value = int(sudoku_output[r, c])
+                cell_data['constraints'].append(set_value)
+            else:
+                cell_data['constraints'] = list(range(1, 10))
+            
+            #assign the value to the cell in the look ahead table
+            sudoku_look_ahead_table[r, c] = cell_data  # Assign a new list to each cell
+    
+    #run make move to update the sudoku lookahead table
+    for r in range(9):
+        for c in range(9):
+            if(sudoku_output[r, c] > 0):
+                make_move(r, c, int(sudoku_output[r, c]), sudoku_look_ahead_table, sudoku_output)
+
+    for r in range(9):
+        for c in range(9):
+            print(sudoku_look_ahead_table[r, c]['constraints'])
+    return jsonify(sudoku_output.tolist())
 
 if __name__ == '__main__':
     app.run(debug=True)
